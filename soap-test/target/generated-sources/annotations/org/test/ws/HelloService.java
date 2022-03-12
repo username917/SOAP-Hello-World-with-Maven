95c915f8566edea09d09497a95f5482f4b34b7f1
username917 <86450720+username917@users.noreply.github.com>
2022-03-12 16:55:43 -0500

    Add files via upload

---
+++ soap-test/target/generated-sources/annotations/org/test/ws/HelloService.java
@@ -0,0 +1,17 @@
+package org.test.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public class HelloService 
+{
+  @WebMethod(operationName = "sayHello")
+  public String sayHello(@WebParam(name="guestname") String guestname) {
+    if (guestname == null) { return "Hello"; }
+    return "Hello " + guestname;
+  }
+}
